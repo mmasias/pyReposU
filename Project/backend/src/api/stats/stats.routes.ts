18035e824808e0603ff05f2fe3cf40f5f19162ac
juanjoc711 <114565115+juanjoc711@users.noreply.github.com>
2025-02-03 14:08:18 +0100

    merge(SolucionadoErrorRutas):añadidas las rutas faltantes tras el merge)

--- Project/backend/src/api/stats/stats.routes.ts
+++ Project/backend/src/api/stats/stats.routes.ts
@@ -2,11 +2,13 @@
 import { Router } from 'express';
 import folderRoutes from './folders/folderRoutes'; 
 import userRoutes from './user/UserStats.routes';
+import { getRepositoryTree } from './folders/folderTree.controller'; // ✅ Importar la función
 
 const router = Router();
 
 // Conectar las rutas de cada submódulo
-router.use('/folders', folderRoutes); // Rutas de estadísticas por carpetas
-router.use('/user', userRoutes); // Rutas de estadísticas por usuario
+router.use('/folders', folderRoutes); 
+router.use('/user', userRoutes); 
+router.get('/tree', getRepositoryTree); 
 
 export default router;
