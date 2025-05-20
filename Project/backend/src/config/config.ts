55b1bdf21e1c506d06e31b1389ea92378c068e66
juanjoc711 <114565115+juanjoc711@users.noreply.github.com>
2025-05-20 15:54:03 +0200

    fix(solucionado el bug lf/CRLF del dockerizado): terminado de solucionar el bug causado  por los archivos .sh.

--- Project/backend/src/config/config.ts
+++ Project/backend/src/config/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const config = {
   server: {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 5000,
   },
   github: {
     token: process.env.GITHUB_TOKEN || "",
